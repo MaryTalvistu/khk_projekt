f97e8eaf69b3a503d23043b6d5e538ddb36199a9
mary-ann.talvistu <mary-ann.talvistu@khk.ee>
2020-12-11 12:53:16 +0200

    modified file `wflogs` (after activation of `Wordfence Security` version 7.4.14)

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1608288792;s:7:"authKey";s:64:"r`A4y>FFY|H|o8D@X`tAxLjSQ>5NA?KJ+<6ay-q0<y< vb0z?2]6JBf1wW[I2azR";}
