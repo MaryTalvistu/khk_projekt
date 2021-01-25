26a0b95b0f00b9eea1bb7783c02d91e644585855
mary-ann.talvistu <mary-ann.talvistu@khk.ee>
2021-01-25 02:28:10 +0200

    modified file `config.php` (after deactivation of `WP Activity Log` version 4.1.5.1)

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"r`A4y>FFY|H|o8D@X`tAxLjSQ>5NA?KJ+<6ay-q0<y< vb0z?2]6JBf1wW[I2azR";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:785;}
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"r`A4y>FFY|H|o8D@X`tAxLjSQ>5NA?KJ+<6ay-q0<y< vb0z?2]6JBf1wW[I2azR";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:785;}
